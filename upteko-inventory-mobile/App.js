733662dad269fed674cb8bef251a0aca4e46d6ec
Alex <nguyenalexxx99@gmail.com>
2024-02-19 12:42:24 +0100

    able to access camera with mobile app

--- upteko-inventory-mobile/App.js
+++ upteko-inventory-mobile/App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { QRCodeScanner } from './QRCodeScanner';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   {/* Render the QRCodeScanner component */}
+    //   <QRCodeScanner />
+    //   <StatusBar style="auto" />
+    // </View>
+    <QRCodeScanner/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
